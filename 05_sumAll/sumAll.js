9aadd946857def55664f9f0b2899b7148b84e751
Thuan Bui <9248622+10h30@users.noreply.github.com>
2025-02-22 10:40:23 +0900

    Completed 1-5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    if (a > b) {
+        min = b;
+        max = a;
+    }
+    else {
+        min = a;
+        max = b;
+    }
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
+    for (let i = min; i<=max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
