478676904fdd1984abec95fc8aaceffa7b40ac05
Thuan Bui <9248622+10h30@users.noreply.github.com>
2025-02-22 10:53:29 +0900

    Complete 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeinF) {
+  degreeinC = Math.round(((degreeinF  - 32) / 1.8) * 10)/10
+  return degreeinC
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeinC) {
+  degreeinF = Math.round(((degreeinC* 1.8) + 32) * 10)/10
+  return degreeinF
 };
 
 // Do not edit below this line
