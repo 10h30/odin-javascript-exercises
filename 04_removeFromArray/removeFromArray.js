c4153821372564b7489f0acaf3f52024ed992dbd
Thuan Bui <9248622+10h30@users.noreply.github.com>
2025-02-22 10:23:40 +0900

    Completed 1-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,38 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray,...theArgs) {
+    let newArray = [];
+    console.log("Original Array: " + originalArray)
+    console.log("Argument Length: " + theArgs.length)
+    /*for (const number of originalArray) {
+        console.log("Current number: " + number)
+        for (let i=0; i < arguments.length; i++) {
+            console.log("Current argument value: " + theArgs[i]);
+            if (number === theArgs[i])  {
+                const index = originalArray.indexOf(number);
+                if (index > -1) { // only splice array when item is found
+                    originalArray.splice(index, 1); // 2nd parameter means remove one item only
+                }
+                console.log(newArray)
+            }
+            
+        }
+    }*/
+
+    for (let i=0; i < theArgs.length; i++) {
+        for (z=0; z < originalArray.length; z++ ) {
+        //for (const number of originalArray) {
+            number = originalArray[z];
+            console.log("Current number: " + number);
+            if (number === theArgs[i])  {
+                const index = originalArray.indexOf(number);
+                if (index > -1) { // only splice array when item is found
+                    originalArray.splice(index, 1); // 2nd parameter means remove one item only
+                    z--;
+                }
+            }
+            console.log(originalArray)
+        }
+    }
+    return originalArray;
 };
 
 // Do not edit below this line
