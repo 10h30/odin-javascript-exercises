22a6289b629e4b5d7da5bda608f32e6eb8cd3881
Thuan Bui <9248622+10h30@users.noreply.github.com>
2025-02-26 11:16:51 +0900

    Completed excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    console.log(people)
+    const sortByAge = people.sort((a,b) => {
+       
+        if (!a.yearOfDeath) {
+            let currentYear = new Date().getFullYear();            console.log(currentYear)
+            ageOfa = currentYear - a.yearOfBirth
+        }
+        else {
+            ageOfa = a.yearOfDeath - a.yearOfBirth
+        }
+        console.log(a.name + ": " + ageOfa)
 
+        if (!b.yearOfDeath) {
+            let currentYear = new Date().getFullYear();
+            ageOfb = currentYear - b.yearOfBirth
+        }
+        else {
+            ageOfb = b.yearOfDeath - b.yearOfBirth
+        }
+        console.log(b.name + ": " + ageOfb)
+        
+        return ageOfa < ageOfb ? 1 : -1
+    })
+    const oldest = sortByAge[0]
+    console.log(oldest);
+    return oldest;
 };
 
 // Do not edit below this line
