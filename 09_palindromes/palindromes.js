aa6cde3a791362a308bcdf485475105d03927bb3
Thuan Bui <9248622+10h30@users.noreply.github.com>
2025-02-26 10:21:22 +0900

    Complete excersie 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (checkString) {
+    const formattedString = checkString
+                                    .split("")
+                                    .filter(char => {return /[a-zA-Z0-9]/.test(char)})
+                                    .join("");
+    console.log("Original: " + formattedString.toUpperCase())
+    const reverseString = formattedString
+                                .split("")
+                                .reverse()
+                                .join("");
+    console.log("Reverse: " + reverseString.toUpperCase())
+    return (formattedString.toUpperCase() === reverseString.toUpperCase())
 };
 
 // Do not edit below this line
