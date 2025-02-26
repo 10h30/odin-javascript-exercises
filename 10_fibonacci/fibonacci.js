0a70d76792394a4c7e98bd96aaae63174aa5ce15
Thuan Bui <9248622+10h30@users.noreply.github.com>
2025-02-26 10:44:12 +0900

    Complete excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(number) {   
+    console.log(number)
+    if (typeof number == "string") {
+        number = parseInt(number)
+    }
+    const array = []
+    for (i=0; i<number; i++) {
+        if (i < 2) {
+            let newNumber = 1
+            array.push(newNumber)
+        }
+        else {
+            let newNumber = array[i-2] + array [i-1]
+            array.push(newNumber)
+        }
+    }
+    console.log("Array: " + array)
+    if (number < 0) {
+        return "OOPS"
+    }
+    else {
+        return number === 0 ? 0 : array[number-1]
+    }
 
 };
 
